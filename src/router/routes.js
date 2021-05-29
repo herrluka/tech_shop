6abbb96530a698815ae42967ad11cbd0813998d2
Luka <luka.radujevic@gmail.com>
2021-05-29 15:41:01 +0200

    Added route protection

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,9 +3,20 @@ import PageNotFound from "../components/PageNotFound";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import Cart from "../components/Cart";
+import {store} from "../vuex-store";
+
+function isEmployee() {
+  const user = store.state.user;
+  return user && user.role === 'employee';
+}
+
+function isCustomer() {
+  const user = store.state.user;
+  return user && user.role === 'customer';
+}
 
 const routes = [
-  { path: '/cart', component: Cart},
+  { path: '/cart', component: Cart, beforeEnter: (to, from, next) => isCustomer() ? next() : next('/login')},
   { path: '/register', component: Register},
   { path: '/login', component: Login},
   { path: '/', component: Home},
